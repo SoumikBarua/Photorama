e2bb6ca33b04161e7ab17693280691cacc62c290
SB <sb@SBs-MacBook-Pro.fios-router.home>
2019-01-02 21:37:29 -0500

    Initial commit, replaced the default view controller with PhotosViewController, embedded PhotosViewController in a navigation controller and added an image view to it

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by SB on 1/2/19.
+//  Copyright © 2019 SB. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
