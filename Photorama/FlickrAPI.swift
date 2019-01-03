af7f7682da5f97d8cc785351ff9992b22aa84f61
SB <sb@SBs-MacBook-Pro.fios-router.home>
2019-01-02 22:30:12 -0500

    Added a new file FlickrAPI.swift to implement handling URLComponents functionality for sending web service request

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,51 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by SB on 1/2/19.
+//  Copyright © 2019 SB. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case interestingPhotos = "flickr.interestingness.getList"
+}
+
+struct FlickrAPI {
+    
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let apiKey = "a6d819499131071f158fd740860a5a88"
+    
+    private static func flickrURL(method: Method, parameters: [String:String]?) -> URL {
+        var components = URLComponents(string: baseURLString)!
+        
+        var queryItems = [URLQueryItem]()
+        
+        let baseParams = [
+            "method": method.rawValue,
+            "format": "json",
+            "nojsoncallback": "1",
+            "api_key": apiKey
+        ]
+        
+        for (key, value) in baseParams {
+            let item = URLQueryItem(name: key, value: value)
+            queryItems.append(item)
+        }
+        
+        if let additionalParams = parameters {
+            for (key, value) in additionalParams {
+                let item = URLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+        
+        return components.url!
+    }
+    
+    static var interestingPhotosURL: URL {
+        return flickrURL(method: .interestingPhotos, parameters: ["extras": "url_h,date_taken"])
+    }
+}
