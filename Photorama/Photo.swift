e5ca1e4823302b9d841eb3fa10f02646f670026f
SB <sb@SBs-MacBook-Pro.local>
2019-01-03 17:04:53 -0500

    Added a new class Photo to represent each photo returned from the web service request, parsed string representation of JSON data from the web service using JSONSerialization, used associated values of enums for PhotosResult and updated fetchInterestingPhotos() method signature to take in a completion closure to address the asynchronous process of fetching data

---
+++ Photorama/Photo.swift
@@ -0,0 +1,24 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by SB on 1/3/19.
+//  Copyright © 2019 SB. All rights reserved.
+//
+
+import Foundation
+
+class Photo {
+    
+    let title: String
+    let remoteURL: URL
+    let photoID: String
+    let dateTaken: Date
+    
+    init(title: String, remoteURL: URL, photoID: String, dateTaken: Date) {
+        self.title = title
+        self.remoteURL = remoteURL
+        self.photoID = photoID
+        self.dateTaken = dateTaken
+    }
+}
