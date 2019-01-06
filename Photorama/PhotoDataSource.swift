7f67576b4228f3a8a6e73ba66188b230ff03560e
SB <sb@SBs-MacBook-Pro.fios-router.home>
2019-01-05 20:31:00 -0500

    Replaced existing image view with a collection view on storyboard and set an identifier for its cell, added a new class PhotoDataSource to serve as the data source for the collection view and implemented two required methods, also updated the PhotosViewController to accomodate these changes and customized the layout of the collection view using the size inspector

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by SB on 1/5/19.
+//  Copyright © 2019 SB. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        
+        return cell
+    }
+}
