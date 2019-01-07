16e42ebb0731d22a5e16427341d60eb1fec4e0ad
SB <sb@SBs-MacBook-Pro.local>
2019-01-07 15:48:23 -0500

    Added ImageStore class for image caching, and updated PhotoStore for caching

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -24,6 +24,8 @@ enum PhotosResult {
 
 class PhotoStore {
     
+    let imageStore = ImageStore()
+    
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
         return URLSession(configuration: config)
@@ -60,6 +62,14 @@ class PhotoStore {
     
     func fetchImage(for photo: Photo, completion: @escaping (ImageResult) -> Void) {
         
+        let photoKey = photo.photoID
+        if let image = imageStore.image(forKey: photoKey) {
+            OperationQueue.main.addOperation {
+                completion(.success(image))
+            }
+            return
+        }
+        
         let photoURL = photo.remoteURL
         let request = URLRequest(url: photoURL)
         
@@ -73,6 +83,11 @@ class PhotoStore {
             }
             
             let result = self.processImageRequest(data: data, error: error)
+            
+            if case let.success (image) = result {
+                self.imageStore.setImage(image, forkey: photoKey)
+            }
+            
             OperationQueue.main.addOperation {
                 completion(result)
             }
