92fee37d090bbd10816e6fc3062fb69818ef216f
SB <sb@SBs-MacBook-Pro.local>
2019-01-08 16:40:32 -0500

    Added a new class PhotoInfoViewController to navigate to a photo when user taps on one of the collection view cells, this new view controller will be presented using storyboard segue and updated PhotosViewController accordingly

---
+++ Photorama/PhotoInfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by SB on 1/7/19.
+//  Copyright © 2019 SB. All rights reserved.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var photo: Photo! {
+        didSet{
+            navigationItem.title = photo.title
+        }
+    }
+    var store: PhotoStore!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        store.fetchImage(for: photo) { (result) -> Void in
+            switch result {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error fetching image for photo: \(error)")
+            }
+        }
+    }
+}
