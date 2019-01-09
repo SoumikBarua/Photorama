bec40c1ea8f2443e5f4d267d8212bc92d7bbd013
SB <sb@SBs-MacBook-Pro.local>
2019-01-08 19:02:36 -0500

    Began implementing Core Data, created a new data model called Photorama and added Photo as an entity with its attributes, allowed Core Data to perform attribute transformation using custom class NSURL and to generate NSManageObject subclasses using the model file, created an instance of NSPersistentContainer for data storage and used NSManagedObjectContext to created new entities

---
+++ Photo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by SB on 1/8/19.
+//  Copyright © 2019 SB. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
